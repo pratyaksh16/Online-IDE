141ded5310e5c1266ba6005ab470d02d74b8ba65
Pratyaksh <imaginatorypratyaksh@gmail.com>
2022-06-24 11:03:36 +0530

    feat: Header contents added.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,20 @@ export default function Header(props) {
       <div className="header-sub-container">
         <div className="codeg-logo"><img src="/icons/CodeG-Logo.png" alt="CodeG Logo" /></div>
         <div className="header-content">
+          <span className="logo-text">CODE<span>G</span></span>
           <Dropdown changeEditorLang={props.changeEditorLang} languages={props.languages} currLang={props.currLang}/>
+          <form className="file-name-textbox">
+            <input className="file-name" type="text" placeholder="Untitled" />
+            <button className="file-name-button">Save</button>
+          </form>
+          <div className="user-info">
+            <div className="sign-in">
+              Sign In
+            </div>
+            <div className="sign-up">
+              Sign Up
+            </div>
+          </div>
         </div>
       </div>
     </div>
